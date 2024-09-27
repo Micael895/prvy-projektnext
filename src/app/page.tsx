7375df99c7b9ba997121a82bc099072b7856910f
Micael895 <michal.maczek11@gmail.com>
2024-09-27 07:57:44 +0200

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+  return (
+   <Typography variant="h5" gutterBottom>
+   ZDRAVIM!
+   </Typography>
+   )
+
+ } 
