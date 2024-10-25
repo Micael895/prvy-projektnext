90830f97da67c15e123f97db3f1f3ef551105446
Micael895 <michal.maczek11@gmail.com>
2024-10-25 09:01:14 +0200

    build after next auth working

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,17 @@
-// src/app/profil/page.tsx
-
-import Typography from '@mui/material/Typography';
+//src\app\(home)\page.tsx
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/authOptions";
+import AuthHomeView from "../../sections/AuthHomeView";
+import NonAuthHomeView from "../../sections/NonAuthHomeView";
 
 export const metadata = { title: "Domov | ZoškaSnap" };
 
-export default function Home() {
-  
-  return (
-   
-     <Typography> Domovska stranka </Typography>
-   
-  );
-} 
+export default async function HomePage() {
+  // Fetch session on the server
+  const session = await getServerSession(authOptions);
 
-
-
+  // Conditionally render authenticated or non-authenticated home view
+  return session ? <AuthHomeView session={session} /> : <NonAuthHomeView />;
+}
 
 
